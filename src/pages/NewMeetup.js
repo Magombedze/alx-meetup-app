1363c404a8f85a07e2523f79f818e7d6d77e14b7
Ronnie Magombedze <mhizhawetswanda@gmail.com>
2023-09-29 14:20:16 +0200

    seen

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,29 @@
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetup";
+
+function NewMeetupsPage() {
+  const history = useHistory();
+  function onAddMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-a9e55-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupsPage;
